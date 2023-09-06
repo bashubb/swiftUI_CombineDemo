25c3b1878073cafb79dcaf66ceaec9e9ef250354
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-09-06 14:25:39 +0200

    Initial Commit

---
+++ CombineDemo/CombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDemoApp.swift
+//  CombineDemo
+//
+//  Created by HubertMac on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
